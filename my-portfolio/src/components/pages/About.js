6f9e30407673e830ce3e7ab6297cc1ec5826a0a2
aalexandr6 <aalexanderp6@gmail.com>
2023-05-06 16:04:52 -0500

    worked with tutor & bootrap is better for requirments

---
+++ my-portfolio/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+        <h1>About Pages</h1>
+        <p>
+        I'm an up and coming Web Developer here in Kansas City! I've studied Business Administration and currently work Accounts Payable at a trucking company. I have always loved computers and design and love the idea of turning complex problems into beatiful interface
+        </p>
+    </div>
+  );
+}
--- my-portfolio/src/components/pages/PortfolioContainer.js
+++ my-portfolio/src/components/pages/PortfolioContainer.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
-import NavTabs from './components/pages/NavTabs';
-import Projects from './components/pages/Projects';
-import Contact from './components/pages/Contact';
-import About from './components/pages/About';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
 
-
-
-export default function App() {
-  const [currentPage, setCurrentPage] = useState('About');
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Projects') {
-      return <Projects />;
+    if (currentPage === 'Blog') {
+      return <Blog />;
     }
     return <Contact />;
   };
